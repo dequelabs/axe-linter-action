d2997fba3b17d6a1c92f1bd7dbffdc64e0f613f5
Michael <me@michaelsiek.com>
2022-12-06 14:20:09 -0500

    chore: move script to shell file (#14)

---
+++ axe-linter.sh
@@ -0,0 +1,54 @@
+#! /bin/bash 
+Files="$CHANGED_FILES"
+ApiKey="$API_KEY"
+AxeLinterUrl="$AXE_LINTER_URL"
+
+if [ -z "$AxeLinterUrl" ]; then
+  AxeLinterUrl="https://axe-linter.deque.com/lint-source"
+fi
+
+LinterConfig={}
+if [ -f axe-linter.yml ]; then
+  CheckLinterConfig="$(yq axe-linter.yml)"
+  if [ -n "$CheckLinterConfig" ]; then
+    LinterConfig="$(yq -o=json axe-linter.yml)"
+  fi
+fi
+FoundErrors="0"
+for File in $Files; do
+  FileContents="$(cat "$File")"
+  RequestBody=$(
+    jq \
+      --null-input \
+      --arg Source "$FileContents" \
+      --arg Filename "$File" \
+      --argjson Config "$LinterConfig" \
+      '{ "source": $Source, "filename": $Filename, "config": $Config }'
+  )
+  Response=$(
+    curl \
+      --silent \
+      --request POST \
+      --url "$AxeLinterUrl" \
+      --header "content-type: application/json" \
+      --header "authorization: $ApiKey" \
+      --data "${RequestBody}"
+  )
+  if [ $(echo "$Response" | jq 'has("error")') = "true" ]; then
+    $(echo "$Response" | jq -r '.error')
+    exit 1
+  fi
+  ErrorCount=$(echo "$Response" | jq '.report.errors | length')
+  if [ "$ErrorCount" != "0" ]; then
+    ((FoundErrors += ErrorCount))
+  fi
+  echo "$Response" |
+    jq -r --compact-output '.report.errors[] | .ruleId + " " + (.lineNumber|tostring) + " " + (.column|tostring) + " " + (.endColumn|tostring) + " " + .description' |
+    while read -r RuleId Line Column EndColumn Description; do
+      echo "::error file=$File,line=$Line,col=$Column,endColumn=$EndColumn,title=Axe Linter::$RuleId - $Description"
+    done
+done
+echo "::debug::Found $FoundErrors errors"
+if [ "$FoundErrors" != "0" ]; then
+  exit 1
+fi
