e7095ec711c2065017d1156c604005323482fb3d
michael-siek <me@michaelsiek.com>
2025-02-08 10:16:25 -0500

    feat: convert action to use typescript

--- .husky/build-changed-files.sh
+++ .husky/build-changed-files.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# only run the npm build script for each workspace directory that has changed files
-function run_build {
-  # convert multiline string into array
-  # @see https://stackoverflow.com/questions/24628076/convert-multiline-string-to-array
-  local IFS=$'\n'
-  local lines=($1)
-  local i
-
-  # get only the unique values from an array
-  # `lines[$i]%%/*` gets the substring up to the first '/'. so `semantic-pr-footer-v1/src/run.ts` becomes `semantic-pr-footer-v1`
-  # @see https://www.baeldung.com/linux/bash-unique-values-arrays
-  unique_dirs=($(for ((i = 0; i < ${#lines[@]}; i++)); do echo "${lines[$i]%%/*}"; done | sort -u))
-
-  for dir in "${unique_dirs[@]}"; do
-    yarn build -w "$dir"
-  done
-}
-
 cd "$(dirname -- "$0")/../."
 yarn build
 
