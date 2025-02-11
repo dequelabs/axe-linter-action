e09f63323cb48b391f7fe98a6c1a98a5bcd21a67
michael-siek <me@michaelsiek.com>
2025-02-11 10:31:30 -0500

    update docs

--- dist/index.js
+++ dist/index.js
@@ -43061,9 +43061,6 @@ ${pendingInterceptorsFormatter.format(pending)}
             const errors = result.report.errors
             totalErrors += errors.length
             // Report errors using GitHub annotations
-            // There is a limit of 10 warning annotations and 10 error annotations per step
-            // If there are more errors, they will not be reported
-            // https://github.com/orgs/community/discussions/26680
             for (const error of errors) {
               core.error(`${error.ruleId} - ${error.description}`, {
                 file,
