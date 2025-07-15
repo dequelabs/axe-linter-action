c0d7c0ee86dbb50720615a6f821f239623b22b4a
michael-siek <me@michaelsiek.com>
2025-07-15 09:44:07 -0400

    chore(release): 1.2.0

--- dist/index.js
+++ dist/index.js
@@ -4920,7 +4920,7 @@
       var import_universal_user_agent = __nccwpck_require__(3843)
 
       // pkg/dist-src/version.js
-      var VERSION = '9.0.5'
+      var VERSION = '9.0.6'
 
       // pkg/dist-src/defaults.js
       var userAgent = `octokit-endpoint.js/${VERSION} ${(0, import_universal_user_agent.getUserAgent)()}`
@@ -5044,9 +5044,9 @@
       }
 
       // pkg/dist-src/util/extract-url-variable-names.js
-      var urlVariableRegex = /\{[^}]+\}/g
+      var urlVariableRegex = /\{[^{}}]+\}/g
       function removeNonChars(variableName) {
-        return variableName.replace(/^\W+|\W+$/g, '').split(/,/)
+        return variableName.replace(/(?:^\W+)|(?:(?<!\W)\W+$)/g, '').split(/,/)
       }
       function extractUrlVariableNames(url) {
         const matches = url.match(urlVariableRegex)
@@ -5259,7 +5259,7 @@
           if (url.endsWith('/graphql')) {
             if (options.mediaType.previews?.length) {
               const previewsFromAcceptHeader =
-                headers.accept.match(/[\w-]+(?=-preview)/g) || []
+                headers.accept.match(/(?<![\w-])[\w-]+(?=-preview)/g) || []
               headers.accept = previewsFromAcceptHeader
                 .concat(options.mediaType.previews)
                 .map((preview) => {
@@ -5528,7 +5528,7 @@
       module.exports = __toCommonJS(dist_src_exports)
 
       // pkg/dist-src/version.js
-      var VERSION = '9.2.1'
+      var VERSION = '9.2.2'
 
       // pkg/dist-src/normalize-paginated-list-response.js
       function normalizePaginatedListResponse(response) {
@@ -5580,7 +5580,7 @@
                 const normalizedResponse =
                   normalizePaginatedListResponse(response)
                 url = ((normalizedResponse.headers.link || '').match(
-                  /<([^>]+)>;\s*rel="next"/
+                  /<([^<>]+)>;\s*rel="next"/
                 ) || [])[1]
                 return { value: normalizedResponse }
               } catch (error) {
@@ -8371,7 +8371,7 @@
           if (options.request.headers.authorization) {
             requestCopy.headers = Object.assign({}, options.request.headers, {
               authorization: options.request.headers.authorization.replace(
-                / .*$/,
+                /(?<! ) .*$/,
                 ' [REDACTED]'
               )
             })
@@ -8448,7 +8448,7 @@
       var import_universal_user_agent = __nccwpck_require__(3843)
 
       // pkg/dist-src/version.js
-      var VERSION = '8.4.0'
+      var VERSION = '8.4.1'
 
       // pkg/dist-src/is-plain-object.js
       function isPlainObject(value) {
@@ -8524,7 +8524,7 @@
             if ('deprecation' in headers) {
               const matches =
                 headers.link &&
-                headers.link.match(/<([^>]+)>; rel="deprecation"/)
+                headers.link.match(/<([^<>]+)>; rel="deprecation"/)
               const deprecationLink = matches && matches.pop()
               log.warn(
                 `[@octokit/request] "${requestOptions.method} ${requestOptions.url}" is deprecated. It is scheduled to be removed on ${headers.sunset}${deprecationLink ? `. See ${deprecationLink}` : ''}`
@@ -22519,7 +22519,7 @@
       'use strict'
 
       const { parseSetCookie } = __nccwpck_require__(8915)
-      const { stringify, getHeadersList } = __nccwpck_require__(3834)
+      const { stringify } = __nccwpck_require__(3834)
       const { webidl } = __nccwpck_require__(4222)
       const { Headers } = __nccwpck_require__(6349)
 
@@ -22595,16 +22595,13 @@
 
         webidl.brandCheck(headers, Headers, { strict: false })
 
-        const cookies = getHeadersList(headers).cookies
+        const cookies = headers.getSetCookie()
 
         if (!cookies) {
           return []
         }
 
-        // In older versions of undici, cookies is a list of name:value.
-        return cookies.map((pair) =>
-          parseSetCookie(Array.isArray(pair) ? pair[1] : pair)
-        )
+        return cookies.map((pair) => parseSetCookie(pair))
       }
 
       /**
@@ -23047,16 +23044,13 @@
       /***/
     },
 
-    /***/ 3834: /***/ (
-      module,
-      __unused_webpack_exports,
-      __nccwpck_require__
-    ) => {
+    /***/ 3834: /***/ (module) => {
       'use strict'
 
-      const assert = __nccwpck_require__(2613)
-      const { kHeadersList } = __nccwpck_require__(6443)
-
+      /**
+       * @param {string} value
+       * @returns {boolean}
+       */
       function isCTLExcludingHtab(value) {
         if (value.length === 0) {
           return false
@@ -23327,31 +23321,13 @@
         return out.join('; ')
       }
 
-      let kHeadersListNode
-
-      function getHeadersList(headers) {
-        if (headers[kHeadersList]) {
-          return headers[kHeadersList]
-        }
-
-        if (!kHeadersListNode) {
-          kHeadersListNode = Object.getOwnPropertySymbols(headers).find(
-            (symbol) => symbol.description === 'headers list'
-          )
-
-          assert(kHeadersListNode, 'Headers cannot be parsed')
-        }
-
-        const headersList = headers[kHeadersListNode]
-        assert(headersList)
-
-        return headersList
-      }
-
       module.exports = {
         isCTLExcludingHtab,
-        stringify,
-        getHeadersList
+        validateCookieName,
+        validateCookiePath,
+        validateCookieValue,
+        toIMFDate,
+        stringify
       }
 
       /***/
@@ -27636,6 +27612,7 @@
       const { kEnumerableProperty } = __nccwpck_require__(3440)
       const { makeIterator, isValidHeaderName, isValidHeaderValue } =
         __nccwpck_require__(5523)
+      const util = __nccwpck_require__(9023)
       const { webidl } = __nccwpck_require__(4222)
       const assert = __nccwpck_require__(2613)
 
@@ -28203,6 +28180,9 @@
         [Symbol.toStringTag]: {
           value: 'Headers',
           configurable: true
+        },
+        [util.inspect.custom]: {
+          enumerable: false
         }
       })
 
@@ -37961,6 +37941,20 @@ ${pendingInterceptorsFormatter.format(pending)}
             ? { ...options.interceptors }
             : undefined
           this[kFactory] = factory
+
+          this.on('connectionError', (origin, targets, error) => {
+            // If a connection error occurs, we remove the client from the pool,
+            // and emit a connectionError event. They will not be re-used.
+            // Fixes https://github.com/nodejs/undici/issues/3895
+            for (const target of targets) {
+              // Do not use kRemoveClient here, as it will close the client,
+              // but the client cannot be closed in this state.
+              const idx = this[kClients].indexOf(target)
+              if (idx !== -1) {
+                this[kClients].splice(idx, 1)
+              }
+            }
+          })
         }
 
         [kGetDispatcher]() {
